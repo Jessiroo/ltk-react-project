d5ff701e18219f96adbd1e611346f9ded90f0394
Jessiroo <jessie.madsen@gmail.com>
2023-09-04 20:40:08 -0600

    minor fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
-        React Skills test
+        React Skills Test
       </Typography>
     </Box>
   );
